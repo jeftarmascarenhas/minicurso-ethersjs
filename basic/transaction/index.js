9bb006584ee01c80fbb62bec103e2642255b5dd9
jeff mascarenhas <jeftarmascarenhas@hotmail.com>
2022-11-14 22:21:13 -0300

    feat: transaction

---
+++ basic/transaction/index.js
@@ -0,0 +1,38 @@
+require("dotenv").config({ path: "../../.env" });
+const { ethers } = require("ethers");
+
+const API_KEY_TEST_NETWORK = process.env.API_KEY_TEST_NETWORK;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+const provider = new ethers.providers.AlchemyProvider(
+  "goerli",
+  API_KEY_TEST_NETWORK
+);
+
+const main = async () => {
+  const wallet = new ethers.Wallet(PRIVATE_KEY);
+  const signer = wallet.connect(provider);
+  const signerAddress = await signer.getAddress();
+  const gasPrice = await signer.getGasPrice();
+  const gasLimit = ethers.utils.hexlify(21000);
+  const nonce = await signer.getTransactionCount();
+  const value = ethers.utils.parseEther("0.00001");
+
+  const toAddress = "0x189c444645c64E9402F12C75debBfcEc9502c046";
+
+  const tx = {
+    from: signerAddress,
+    to: toAddress,
+    gasPrice,
+    gasLimit,
+    nonce,
+    value,
+  };
+
+  const pendingTx = await signer.sendTransaction(tx);
+  console.log("pendingTx: ", pendingTx);
+  const finishedTx = await pendingTx.wait();
+  console.log("finishedTx: ", finishedTx);
+};
+
+main();
